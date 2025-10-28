a289889a8658e053b46df994aefe0041502317fc
jarkad <jarkad@jarkad.net.eu.org>
2025-10-28 19:46:35 +0100

    WORKS

--- src/hi_world/src/robot.cpp
+++ src/hi_world/src/robot.cpp
@@ -9,22 +9,24 @@ using namespace std::chrono_literals;
 class HiWorld : public rclcpp::Node {
 public:
   HiWorld() : Node("hi_world") {
-    publisher_ =
+    joint_pub_ =
         create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
 
     timer_ = create_wall_timer(33ms, [this]() {
       auto msg = sensor_msgs::msg::JointState();
       msg.header.stamp = get_clock()->now();
       msg.name = {"swivel", "tilt", "periscope"}; // joints being published
-      msg.position = {0, 0, 0};                   // joints values
-      publisher_->publish(msg);
-      RCLCPP_INFO(get_logger(), "Published: %lf %lf %lf", msg.position[0],
-                  msg.position[1], msg.position[2]);
+      msg.position = {counter_++, counter_++, counter_++}; // joints values
+      joint_pub_->publish(msg);
+      // RCLCPP_INFO(get_logger(), "Published: %lf %lf %lf", msg.position[0],
+      //             msg.position[1], msg.position[2]);
     });
   }
 
-  std::shared_ptr<rclcpp::Publisher<sensor_msgs::msg::JointState>> publisher_;
+  std::shared_ptr<rclcpp::Publisher<sensor_msgs::msg::JointState>> joint_pub_;
   std::shared_ptr<rclcpp::TimerBase> timer_;
+
+  double counter_ = 0;
 };
 
 int main(int argc, char **argv) {
