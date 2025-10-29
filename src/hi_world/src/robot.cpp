aca08c0de9270a779690eaf76fb9644ac515536e
jarkad <jarkad@jarkad.net.eu.org>
2025-10-29 22:25:29 +0100

    webots-use-c-api

--- src/hi_world/src/robot.cpp
+++ src/hi_world/src/robot.cpp
@@ -1,7 +1,13 @@
+#include <cmath>
 #include <geometry_msgs/msg/twist.hpp>
+#include <rclcpp/logger.hpp>
+#include <rclcpp/logging.hpp>
 #include <rclcpp/qos.hpp>
+#include <webots/Device.hpp>
 #include <webots/Motor.hpp>
 #include <webots/Robot.hpp>
+#include <webots/motor.h>
+#include <webots/robot.h>
 #include <webots_ros2_driver/PluginInterface.hpp>
 #include <webots_ros2_driver/WebotsNode.hpp>
 
@@ -12,22 +18,24 @@ class RobotDriver : public webots_ros2_driver::PluginInterface {
             [[maybe_unused]] std::unordered_map<std::string, std::string>
                 &parameters) override {
 
+    node_ = node;
+
     RCLCPP_INFO(rclcpp::get_logger("robot_driver"), "hi world");
 
-    webots::Robot *robot = new webots::Robot();
+    wheel_left_ = wb_robot_get_device("left wheel motor");
+    wheel_right_ = wb_robot_get_device("right wheel motor");
 
-    left_wheel_ = robot->getMotor("left wheel motor");
-    right_wheel_ = robot->getMotor("right wheel motor");
+    wb_motor_set_position(wheel_left_, INFINITY);
+    wb_motor_set_position(wheel_right_, INFINITY);
 
-    left_wheel_->setPosition(INFINITY);
-    left_wheel_->setVelocity(0);
+    wb_motor_set_velocity(wheel_left_, 0);
+    wb_motor_set_velocity(wheel_right_, 0);
 
-    right_wheel_->setPosition(INFINITY);
-    right_wheel_->setVelocity(0);
-
-    auto cmd_vel_sub_ = node->create_subscription<geometry_msgs::msg::Twist>(
-        "cmd_vel", rclcpp::SensorDataQoS().reliable(),
+    auto cmd_vel_sub_ = node_->create_subscription<geometry_msgs::msg::Twist>(
+        "/cmd_vel", rclcpp::SensorDataQoS().reliable(),
         [this](std::shared_ptr<geometry_msgs::msg::Twist const> msg) {
+          RCLCPP_INFO(node_->get_logger(), "got twist %lf %lf", msg->linear.x,
+                      msg->angular.z);
           cmd_vel_.linear = msg->linear;
           cmd_vel_.angular = msg->angular;
         });
@@ -40,15 +48,19 @@ class RobotDriver : public webots_ros2_driver::PluginInterface {
     auto fwd = cmd_vel_.linear.x;
     auto rot = cmd_vel_.angular.z;
 
-    left_wheel_->setVelocity((fwd - rot * HALF_WHEEL_SEPARATION) /
-                             WHEEL_RADIUS);
+    auto lvel = (fwd - rot * HALF_WHEEL_SEPARATION) / WHEEL_RADIUS;
+    auto rvel = (fwd + rot * HALF_WHEEL_SEPARATION) / WHEEL_RADIUS;
 
-    right_wheel_->setVelocity((fwd + rot * HALF_WHEEL_SEPARATION) /
-                              WHEEL_RADIUS);
+    wb_motor_set_velocity(wheel_left_, lvel);
+    wb_motor_set_velocity(wheel_right_, rvel);
+
+    RCLCPP_INFO(node_->get_logger(), "step %lf %lf", cmd_vel_.linear.x,
+                cmd_vel_.angular.z);
   }
 
 private:
-  webots::Motor *left_wheel_, *right_wheel_;
+  webots_ros2_driver::WebotsNode *node_;
+  WbDeviceTag wheel_left_, wheel_right_;
   geometry_msgs::msg::Twist cmd_vel_;
 };
 
