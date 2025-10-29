507240ec61ce804b6473c5197ffa490b6fddf833
jarkad <jarkad@jarkad.net.eu.org>
2025-10-29 22:03:38 +0100

    WIP

--- src/hi_world/src/robot.cpp
+++ src/hi_world/src/robot.cpp
@@ -1,37 +1,59 @@
-#include "sensor_msgs/msg/joint_state.hpp"
-#include <cstdio>
-#include <memory>
-#include <rclcpp/executors.hpp>
-#include <rclcpp/node.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <rclcpp/qos.hpp>
+#include <webots/Motor.hpp>
+#include <webots/Robot.hpp>
+#include <webots_ros2_driver/PluginInterface.hpp>
+#include <webots_ros2_driver/WebotsNode.hpp>
 
-using namespace std::chrono_literals;
+namespace hi_world {
 
-class HiWorld : public rclcpp::Node {
-public:
-  HiWorld() : Node("hi_world") {
-    joint_pub_ =
-        create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
+class RobotDriver : public webots_ros2_driver::PluginInterface {
+  void init(webots_ros2_driver::WebotsNode *node,
+            [[maybe_unused]] std::unordered_map<std::string, std::string>
+                &parameters) override {
 
-    timer_ = create_wall_timer(33ms, [this]() {
-      auto msg = sensor_msgs::msg::JointState();
-      msg.header.stamp = get_clock()->now();
-      msg.name = {"swivel", "tilt", "periscope"}; // joints being published
-      msg.position = {counter_++, counter_++, counter_++}; // joints values
-      joint_pub_->publish(msg);
-      // RCLCPP_INFO(get_logger(), "Published: %lf %lf %lf", msg.position[0],
-      //             msg.position[1], msg.position[2]);
-    });
+    RCLCPP_INFO(rclcpp::get_logger("robot_driver"), "hi world");
+
+    webots::Robot *robot = new webots::Robot();
+
+    left_wheel_ = robot->getMotor("left wheel motor");
+    right_wheel_ = robot->getMotor("right wheel motor");
+
+    left_wheel_->setPosition(INFINITY);
+    left_wheel_->setVelocity(0);
+
+    right_wheel_->setPosition(INFINITY);
+    right_wheel_->setVelocity(0);
+
+    auto cmd_vel_sub_ = node->create_subscription<geometry_msgs::msg::Twist>(
+        "cmd_vel", rclcpp::SensorDataQoS().reliable(),
+        [this](std::shared_ptr<geometry_msgs::msg::Twist const> msg) {
+          cmd_vel_.linear = msg->linear;
+          cmd_vel_.angular = msg->angular;
+        });
   }
 
-  std::shared_ptr<rclcpp::Publisher<sensor_msgs::msg::JointState>> joint_pub_;
-  std::shared_ptr<rclcpp::TimerBase> timer_;
+  void step() override {
+    auto constexpr HALF_WHEEL_SEPARATION = 3;
+    auto constexpr WHEEL_RADIUS = 3;
 
-  double counter_ = 0;
+    auto fwd = cmd_vel_.linear.x;
+    auto rot = cmd_vel_.angular.z;
+
+    left_wheel_->setVelocity((fwd - rot * HALF_WHEEL_SEPARATION) /
+                             WHEEL_RADIUS);
+
+    right_wheel_->setVelocity((fwd + rot * HALF_WHEEL_SEPARATION) /
+                              WHEEL_RADIUS);
+  }
+
+private:
+  webots::Motor *left_wheel_, *right_wheel_;
+  geometry_msgs::msg::Twist cmd_vel_;
 };
 
-int main(int argc, char **argv) {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<HiWorld>());
-  rclcpp::shutdown();
-  return 0;
-}
+} // namespace hi_world
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(hi_world::RobotDriver,
+                       webots_ros2_driver::PluginInterface)
