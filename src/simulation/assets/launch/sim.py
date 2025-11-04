0052791f098cb435c7167fc98ce382d35f0bdba8
jarkad <jarkad@jarkad.net.eu.org>
2025-11-04 17:26:18 +0100

    feat: launch simulation with diff drive

--- src/simulation/assets/launch/sim.py
+++ src/simulation/assets/launch/sim.py
@@ -1,35 +1,80 @@
 from launch import LaunchDescription
-from launch.substitutions import PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_path
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
 from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
 from ros_gz_sim.actions import GzServer
 
 
 def generate_launch_description():
-    pkg_simulation_share = FindPackageShare("simulation")
-    gz_gui = ExecuteProcess(cmd=["gz", "sim", "-g", "--render-engine", "ogre"])
+    pkg_simulation_share = get_package_share_path("simulation")
+
+    with open(pkg_simulation_share / "models" / "vehicle" / "model.sdf") as f:
+        robot_sdf = f.read()
 
     return LaunchDescription(
         [
-            GzServer(
-                world_sdf_file=PathJoinSubstitution(
-                    [pkg_simulation_share, "worlds", "vehicle.sdf"]
-                )
+            # arguments
+            DeclareLaunchArgument(
+                "gzgui",
+                default_value="False",
+                description="Open Gazebo GUI.",
             ),
-            gz_gui,
+            DeclareLaunchArgument(
+                "rviz",
+                default_value="False",
+                description="Open RViz2.",
+            ),
+            # simulator
+            GzServer(
+                world_sdf_file=(
+                    pkg_simulation_share / "worlds" / "vehicle.sdf"
+                ).as_posix()
+            ),
+            # ROS<->GZ bridge
+            Node(
+                package="ros_gz_bridge",
+                executable="parameter_bridge",
+                arguments=[
+                    "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+                    "/model/vehicle/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist",
+                    "/model/vehicle/odometry@nav_msgs/msg/Odometry[gz.msgs.Odometry",
+                    "/model/vehicle/pose@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V",
+                    "/model/vehicle/pose_static@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V",
+                ],
+                parameters=[
+                    {"qos_overrides./tf_static.publisher.durability": "transient_local"}
+                ],
+                remappings={
+                    "/model/vehicle/cmd_vel": "/cmd_vel",
+                    "/model/vehicle/pose": "/tf",
+                    "/model/vehicle/pose_static": "/tf_static",
+                }.items(),
+                output="screen",
+            ),
+            # Calculators
             Node(
                 package="robot_state_publisher",
                 executable="robot_state_publisher",
-                output="screen",
-            ),
-            Node(
-                package="ros_gz_bridge",
-                executable="bridge_node",
-                arguments=[
-                    "/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist",
+                parameters=[
+                    {
+                        "robot_description": robot_sdf,
+                        "use_sim_time": True,
+                    }
                 ],
                 output="screen",
+            ),
+            # GUIs
+            ExecuteProcess(
+                cmd=["gz", "sim", "-g", "--render-engine", "ogre"],
+                condition=IfCondition(LaunchConfiguration("gzgui")),
+            ),
+            Node(
+                package="rviz2",
+                executable="rviz2",
+                condition=IfCondition(LaunchConfiguration("rviz")),
             ),
         ]
     )
