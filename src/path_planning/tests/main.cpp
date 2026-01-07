0d483a68d752d3a5ff1974e13dbe59dbaa677c3d
jarkad <jarkad@jarkad.net.eu.org>
2026-01-07 12:20:01 +0000

    feat(path_planning): install Catch2
    Catch2 is a testing framework for C++. colcon natively integrates with
    GTest, but GTest doesn't support benchmarks. I've checked and Catch2
    works just as well with colcon, so that's not a problem either.

---
+++ src/path_planning/tests/main.cpp
@@ -0,0 +1,8 @@
+// Copyright (C) 2026 by RoboTeam Twente
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("hello world", "[path_planning]") {
+    REQUIRE(2 + 2 == 4);
+    REQUIRE_FALSE(2 + 2 == 5);
+}
