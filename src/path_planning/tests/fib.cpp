c75306580b990e241bdfcb64ebe75b217e320c6c
jarkad <jarkad@jarkad.net.eu.org>
2026-01-27 15:16:51 +0000

    [WIP] make tests compile on Humble

---
+++ src/path_planning/tests/fib.cpp
@@ -0,0 +1,13 @@
+// Copyright (C) 2026 by RoboTeam Twente
+
+#include <catch2/catch.hpp>
+#include <cstdint>
+
+uint64_t fib(uint64_t n) { return n < 2 ? 1 : fib(n - 1) + fib(n - 2); }
+
+TEST_CASE("hello world", "[path_planning]") {
+    REQUIRE(2 + 2 == 4);
+    REQUIRE_FALSE(2 + 2 == 5);
+
+    BENCHMARK("fib(20)") { return fib(20); };
+}
