d14c591e658ed6c614e452dee6b9ce2bf45124b1
jarkad <jarkad@jarkad.net.eu.org>
2026-02-18 12:59:46 +0100

    refactor(build): add rtt_ prefix to path_planning

--- src/rtt_path_planning/src/path_planning.cpp
+++ src/rtt_path_planning/src/path_planning.cpp
@@ -1,5 +1,5 @@
 #include <cstdio>
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
-    printf("hello world path_planning package\n");
+    printf("hello world rtt_path_planning package\n");
 }
